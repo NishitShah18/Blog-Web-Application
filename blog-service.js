359090cd6f29572668c72e302abb91d6612c0c18
Nishit Shah <ngshah3@myseneca.ca>
2023-01-15 13:31:22 -0500

    Update blog-service.js

--- blog-service.js
+++ blog-service.js
@@ -1,4 +1,6 @@
 const Sequelize = require("sequelize");
+const env = require("dotenv")
+env.config()
 
 var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
